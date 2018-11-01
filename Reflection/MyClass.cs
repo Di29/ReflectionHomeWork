07a99ef617c57338b1aed1e660319e51dda32e3a
Уринбасаров Диас Нуржанулы <@corp.itstep>
2018-11-01 19:34:43 +0600

    done

---
+++ Reflection/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class MyClass
+    {
+        public int CountOfStudents { get; set; }
+        public string NameOfSubject { get; set; }
+        public string NameOfTeacher { get; set; }
+
+        public MyClass(int count, string nameSub, string NameTeach)
+        {
+            NameOfSubject = nameSub;
+            NameOfTeacher = NameTeach;
+            CountOfStudents = count;
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+        }
+
+        private static void Task2()
+        {
+            MyClass clas = new MyClass(9, "C#", "Oleg Skidan");
+
+            Type type = typeof(MyClass);
+
+            var prop = type.GetProperties();
+
+            foreach (var pr in prop)
+            {
+                Console.WriteLine(pr.Name + " - " + pr.GetValue(clas));
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Task1()
+        {
+            Type type = typeof(Console);
+            int count = 1;
+
+            var methods = type.GetMethods();
+            int length = type.GetMethods().Length;
+            Console.WriteLine("Methods of Console class:");
+
+            foreach (var method in methods)
+            {
+                Console.SetCursorPosition(10, count++);
+                Console.WriteLine("Mетод: {0}", method.Name);
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
